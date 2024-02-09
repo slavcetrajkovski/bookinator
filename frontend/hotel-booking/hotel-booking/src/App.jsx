f4a0c297426e79ea959757b028ace081d1d64fc0
Slavcho <s.trajkovski02@yahoo.com>
2024-02-09 15:00:18 +0100

    Finished the browsing page

--- frontend/hotel-booking/hotel-booking/src/App.jsx
+++ frontend/hotel-booking/hotel-booking/src/App.jsx
@@ -5,19 +5,25 @@ import ExistingRooms from './components/room/ExistingRooms'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import EditRoom from './components/room/EditRoom'
 import Home from './components/home/Home'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import RoomListing from './components/room/RoomListing'
 
 function App() {
   return (
     <>
     <main>
       <Router>
+      <Navbar />
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/edit-room/:roomId" element={<EditRoom />}/>
           <Route path="/existing-rooms" element={<ExistingRooms />}/>
           <Route path="/add-room" element={<AddRoom />}/>
+          <Route path="/browse-all-rooms" element={<RoomListing />}/>
         </Routes>
       </Router>
+      <Footer />
     </main>
     </>
   )
